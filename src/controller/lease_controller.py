62fea2f3bf5a8c45ad73bb0020f8a529960877e0
freelancing-solutions <mobiusndou@gmail.com>
2023-07-04 16:53:16 +0200

    updated lease controller can now create unit invoice

--- src/controller/lease_controller.py
+++ src/controller/lease_controller.py
@@ -1,4 +1,12 @@
-from database.models.invoices import Invoice
+import uuid
+from datetime import datetime, date
+from src.database.models.companies import Company
+from src.database.models.invoices import Invoice
+from src.database.models.tenants import Tenant
+from src.database.sql.tenants import TenantORM
+from src.database.sql.companies import CompanyORM
+from src.database.models.properties import Unit, Property
+from src.database.sql.properties import PropertyORM
 from src.database.models.invoices import Invoice, UnitCharge
 from src.database.sql.invoices import InvoiceORM, ItemsORM, UserChargesORM
 from src.logger import init_logger
@@ -74,21 +82,59 @@ class LeaseController:
             return Invoice(**invoice_orm.to_dict())
 
     @error_handler
-    async def create_invoice(self, invoice_charges: list[UnitCharge], unit_: Unit):
+    async def create_invoice(self, invoice_charges: list[UnitCharge], unit_: Unit) -> InvoiceORM:
         """
 
         :param invoice_charges:
         :param unit_:
         :return:
         """
-        #todo obtain property - using unit_.property_id
-        #todo obtain company details using property
+        # todo obtain property - using unit_.property_id
+        # todo obtain company details using property
         # todo obtain tenant usint unit_.tenant_id
-        service_name: str = f"{property_.name} Invoice"
-        description: str = f"{company.company_name} Monthly Rental Invoice for Property : {property_.name}"
-        _date_issued = datetime.now().date()
-        _due_date = await self.calculate_due_date(date_issued=_date_issued)
-        _invoice_data = dict(service_name=service_name, description=description, currency=profile.currency,
-                             customer=dict(tenant_id=tenant.tenant_id, name=tenant.name, email=tenant.email,
-                                           cell=tenant.cell),
-                             date_issued=_date_issued, due_date=_due_date, invoiced_items=_invoiced_items)
+        with Session() as session:
+            property_orm = session.query(PropertyORM).filter(PropertyORM.property_id == unit_.property_id).first()
+            company_orm = session.query(CompanyORM).filter(CompanyORM).first()
+            tenant_orm = session.query(TenantORM).filter(TenantORM.tenant_id == unit_.tenant_id).first()
+            property_: Property = Property(**property_orm.to_dict())
+            company: Company = Company(**company_orm.to_dict())
+            tenant: Tenant = Tenant(**tenant_orm.to_dict())
+
+            service_name: str = f"{property_.name} Invoice"
+            description: str = f"{company.company_name} Monthly Rental Invoice for Property : {property_.name}"
+            date_issued: date = datetime.now().date()
+            due_date: date = await self.calculate_due_date(date_issued=date_issued)
+            list_charge_ids = await self.get_charge_ids(invoice_charges=invoice_charges)
+            charge_ids: str = ",".join(list_charge_ids)
+
+            invoice_orm: InvoiceORM = InvoiceORM(invoice_number=str(uuid.uuid4()), service_name=service_name,
+                                                 description=description, currency="R", tenant_id=tenant.tenant_id,
+                                                 discount=0, tax_rate=0, date_issued=date_issued, due_date=due_date,
+                                                 month=due_date.month, rental_amount=unit_.rental_amount,
+                                                 charge_ids=charge_ids, invoice_sent=False, invoice_printed=False)
+
+            session.add(invoice_orm)
+            session.commit()
+
+            return invoice_orm
+
+    @staticmethod
+    async def calculate_due_date(date_issued: date) -> date:
+        if date_issued.day >= 7:
+            if date_issued.month == 12:
+                due_date = date(date_issued.year + 1, 1, 7)
+            else:
+                due_date = date(date_issued.year, date_issued.month + 1, 7)
+        else:
+            due_date = date(date_issued.year, date_issued.month, 7)
+
+        return due_date
+
+    @staticmethod
+    async def get_charge_ids(invoice_charges: list[UnitCharge]) -> list[str]:
+        """
+
+        :param invoice_charges:
+        :return:
+        """
+        return [charge.charge_id for charge in invoice_charges if charge] if invoice_charges else []
