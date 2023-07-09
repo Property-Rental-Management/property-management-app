07101dd2a41a53d957bec87b288001b54038b26d
freelancing-solutions <mobiusndou@gmail.com>
2023-07-09 11:45:15 +0200

    added payment models

--- src/routes/payments.py
+++ src/routes/payments.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+
 from src.authentication import login_required
 from src.database.models.users import User
 
@@ -14,3 +15,13 @@ async def get_payments(user: User):
     return render_template('payments/payments.html', **context)
 
 
+@payments_route.get('/admin/get-unit-payment/<string:invoice_number>')
+@login_required
+async def get_unit_payment(user: User):
+    """
+    **get_unit_payment**
+        will return payment form for use in unit payments
+    :param user:
+    :return:
+    """
+    pass
