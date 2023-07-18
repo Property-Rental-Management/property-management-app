c5d15f5422922b974e6a814ea5cc4c0079ce5972
freelancing-solutions <mobiusndou@gmail.com>
2023-07-18 12:44:58 +0200

    refactored statements

--- src/routes/reports.py
+++ src/routes/reports.py
@@ -1,5 +1,4 @@
 
-
 from flask import Blueprint, render_template
 
 from src.authentication import login_required
@@ -21,3 +20,17 @@ async def get_reports(user: User):
     context = dict(user=user_data, companies=companies_dicts)
 
     return render_template('reports/reports.html', **context)
+
+
+@reports_route.get('/admin/report/<string:company_id>')
+@login_required
+async def report(user: User, company_id: str):
+    """
+
+    :param user:
+    :param company_id:
+    :return:
+    """
+    company = await company_controller.get_company_internal(company_id=company_id)
+    context = dict(user=user.dict(), company=company.dict())
+    return render_template('reports/reporting_interface.html', **context)
