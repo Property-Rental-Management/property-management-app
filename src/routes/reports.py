59b781e1e2dd16f9e74eca1155a58f65ce988279
freelancing-solutions <mobiusndou@gmail.com>
2023-07-18 12:36:48 +0200

    refactored statements

--- src/routes/reports.py
+++ src/routes/reports.py
@@ -5,6 +5,7 @@ from flask import Blueprint, render_template
 from src.authentication import login_required
 from src.database.models.users import User
 from src.logger import init_logger
+from src.main import company_controller
 
 reports_route = Blueprint('reports', __name__)
 
@@ -15,7 +16,8 @@ report_logger = init_logger('REPORT LOGGER:')
 @login_required
 async def get_reports(user: User):
     user_data = user.dict()
-    context = dict(user=user_data)
+    companies = await company_controller.get_user_companies(user_id=user.user_id)
+    companies_dicts = [company.dict() for company in companies if company] if companies else []
+    context = dict(user=user_data, companies=companies_dicts)
 
     return render_template('reports/reports.html', **context)
-
